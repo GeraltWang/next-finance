944bbffbb08d2869751c47fad434f06bf28317c5
GeraltWang <wgeralt@outlook.com>
2024-08-20 15:52:41 +0800

    feat: ✨ add progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import { Toaster } from '@/components/ui/sonner'
 import { QueryProvider } from '@/providers/query-provider'
 import { zhCN } from '@clerk/localizations'
 import { ClerkProvider } from '@clerk/nextjs'
-import { Toaster } from '@/components/ui/sonner'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +23,7 @@ export default function RootLayout({
 		<ClerkProvider localization={zhCN}>
 			<html lang='zh-CN'>
 				<body className={inter.className}>
+					<NextTopLoader color='#FF7A00' />
 					<QueryProvider>
 						<Toaster />
 						{children}
