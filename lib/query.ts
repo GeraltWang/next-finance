884eda2e90eaf1782c0702883791b3cfccf16e35
GeraltWang <WGERALT@OUTLOOK.COM>
2024-10-25 17:57:38 +0800

    feat: ✨ add query utils

---
+++ lib/query.ts
@@ -0,0 +1,35 @@
+import { RemoveUrlQueryParams, UrlQueryParams } from '@/types'
+import qs from 'query-string'
+
+export function formUrlQuery({ params, queryParams }: UrlQueryParams) {
+	const currentUrl = qs.parse(params)
+
+	// 合并新的查询参数
+	Object.keys(queryParams).forEach(key => {
+		currentUrl[key] = queryParams[key]
+	})
+
+	return qs.stringifyUrl(
+		{
+			url: window.location.pathname,
+			query: currentUrl,
+		},
+		{ skipNull: true }
+	)
+}
+
+export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
+	const currentUrl = qs.parse(params)
+
+	keysToRemove.forEach(key => {
+		delete currentUrl[key]
+	})
+
+	return qs.stringifyUrl(
+		{
+			url: window.location.pathname,
+			query: currentUrl,
+		},
+		{ skipNull: true }
+	)
+}
--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,19 @@
 export interface UserMeta {
 	userId?: string
 }
+
+// ====== URL QUERY PARAMS
+export interface UrlQueryParams {
+	params: string
+	queryParams: Record<string, any>
+}
+
+export interface RemoveUrlQueryParams {
+	params: string
+	keysToRemove: string[]
+}
+
+export interface SearchParamProps {
+	params: { id: string }
+	searchParams: { [key: string]: string | string[] | undefined }
+}
