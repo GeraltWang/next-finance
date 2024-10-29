e13499c8e797b04a2687096a4c4de65b4e4177a9
GeraltWang <WGERALT@OUTLOOK.COM>
2024-10-29 14:11:38 +0800

    chore: 📦 imports

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -4,9 +4,9 @@ import { SelectSingleEventHandler } from 'react-day-picker'
 
 import { cn } from '@/lib/utils'
 
-import { Button } from './ui/button'
-import { Calendar } from './ui/calendar'
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
 type Props = {
 	value?: Date
